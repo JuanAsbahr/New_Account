ac714fff03aea89bf978a056630988bede1e0501
JuanAsbahr <juan_asbahr@hotmail.com>
2023-07-03 18:52:27 -0300

    Exception

---
+++ Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace New_Account.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
